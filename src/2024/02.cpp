5ed3030f1e94024565c91c705da2bfbdf754460c
ColeTrammer <cole.trammer@gmail.com>
2024-12-01 21:07:07 -0800

    Solve 2024 day 2
          -------Part 1--------   -------Part 2--------
    Day       Time  Rank  Score       Time  Rank  Score
      2   00:04:06   262      0   00:06:26   261      0

---
+++ src/2024/02.cpp
@@ -0,0 +1,64 @@
+#include <di/container/interface/empty.h>
+#include <di/container/tree/tree_map.h>
+#include <di/container/tree/tree_multimap.h>
+#include <di/container/tree/tree_multiset.h>
+#include <di/container/tree/tree_set.h>
+#include <di/container/view/zip.h>
+#include <di/container/view/zip_transform.h>
+#include <di/function/not_fn.h>
+#include <dius/print.h>
+#include <runner/aliases.h>
+#include <runner/aoc_problem_registry.h>
+
+#include "di/container/view/pairwise_transform.h"
+#include "di/function/greater.h"
+#include "di/function/minus.h"
+
+using namespace di;
+using namespace dius;
+
+AOC_SOLUTION(2024, 2, a, i64) {
+    auto lines = input | splitv("\n"_tsv);
+
+    auto s = 0;
+    for (auto [row, line] : enumerate(lines)) {
+        auto ns = all_nums(line, ' ');
+
+        auto ds = ns | pairwise_transform(minus);
+
+        if (all_of(ds, greater(0)) || all_of(ds, less(0))) {
+            if (all_of(ds | transform(abs), [](int x) {
+                    return x >= 1 && x <= 3;
+                })) {
+                s++;
+            }
+        }
+    }
+    return s;
+}
+
+AOC_SOLUTION(2024, 2, b, i64) {
+    auto lines = input | splitv("\n"_tsv);
+
+    auto s = 0;
+    for (auto [row, line] : enumerate(lines)) {
+        auto ns = all_nums(line, ' ');
+
+        for (auto i : range(ns.size())) {
+            auto nss = clone(ns);
+            nss.erase(nss.begin() + i);
+
+            auto ds = nss | pairwise_transform(minus);
+
+            if (all_of(ds, greater(0)) || all_of(ds, less(0))) {
+                if (all_of(ds | transform(abs), [](int x) {
+                        return x >= 1 && x <= 3;
+                    })) {
+                    s++;
+                    break;
+                }
+            }
+        }
+    }
+    return s;
+}
