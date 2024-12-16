48cccd9142793bb7f00cac8231dac545775c6489
ColeTrammer <cole.trammer@gmail.com>
2024-12-15 22:11:55 -0800

    Solve 2024 day 16
          -------Part 1--------   -------Part 2--------
    Day       Time  Rank  Score       Time  Rank  Score
     16   00:12:34   669      0   00:55:36  1590      0

---
+++ src/2024/16.cpp
@@ -0,0 +1,181 @@
+#include "di/container/hash/node/node_hash_set.h"
+#include "di/function/compare_backwards.h"
+#include "runner/aliases.h"
+#include "runner/aoc_problem_registry.h"
+
+using namespace di;
+using namespace dius;
+
+AOC_SOLUTION(2024, 16, a, i64) {
+    auto lines = input | splitv("\n"_tsv);
+
+    auto s = 0_i64;
+    auto srow = 0_usize;
+    auto scol = 0_usize;
+    auto erow = 0_usize;
+    auto ecol = 0_usize;
+    for (auto [row, line] : enumerate(lines)) {
+        for (auto [col, ch] : enumerate(line)) {
+            if (ch == 'S') {
+                srow = row;
+                scol = col;
+            } else if (ch == 'E') {
+                erow = row;
+                ecol = col;
+            }
+        }
+    }
+
+    struct State {
+        i64 score = 0;
+        usize row = 0;
+        usize col = 0;
+        int dr = 0;
+        int dc = 0;
+
+        bool operator==(State const&) const = default;
+        auto operator<=>(State const&) const = default;
+    };
+
+    struct State2 {
+        usize row = 0;
+        usize col = 0;
+        int dr = 0;
+        int dc = 0;
+
+        bool operator==(State2 const&) const = default;
+        auto operator<=>(State2 const&) const = default;
+    };
+
+    Set<State> queue;
+    Set<State2> visited;
+
+    auto add = [&](State state) {
+        if (state.row >= lines.size() || state.col >= lines[0].size()) {
+            return;
+        }
+
+        if (lines[state.row][state.col] == '#') {
+            return;
+        }
+
+        auto state2 = State2 { state.row, state.col, state.dr, state.dc };
+        if (!visited.contains(state2)) {
+            queue.insert(state);
+            visited.insert(state2);
+        }
+    };
+
+    add({ 0, srow, scol, 0, 1 }); // east
+
+    while (!queue.empty()) {
+        auto state = *queue.begin();
+        queue.erase(queue.begin());
+        if (state.row == erow && state.col == ecol) {
+            return state.score;
+        }
+
+        // Turns
+        add({ state.score + 1000, state.row, state.col, state.dc, state.dr });
+        add({ state.score + 1000, state.row, state.col, -state.dc, -state.dr });
+
+        add({ state.score + 1, state.row + state.dr, state.col + state.dc, state.dr, state.dc });
+    }
+
+    return s;
+}
+
+AOC_SOLUTION(2024, 16, b, i64) {
+    auto lines = input | splitv("\n"_tsv);
+
+    auto s = 0_i64;
+    auto srow = 0_usize;
+    auto scol = 0_usize;
+    auto erow = 0_usize;
+    auto ecol = 0_usize;
+    for (auto [row, line] : enumerate(lines)) {
+        for (auto [col, ch] : enumerate(line)) {
+            if (ch == 'S') {
+                srow = row;
+                scol = col;
+            } else if (ch == 'E') {
+                erow = row;
+                ecol = col;
+            }
+        }
+    }
+
+    struct State {
+        i64 score = 0;
+        Set<Coord> tiles;
+        usize row = 0;
+        usize col = 0;
+        int dr = 0;
+        int dc = 0;
+
+        bool operator==(State const& other) const { return this->score == other.score; }
+        auto operator<=>(State const& other) const { return this->score <=> other.score; };
+    };
+
+    struct State2 {
+        usize row = 0;
+        usize col = 0;
+        int dr = 0;
+        int dc = 0;
+
+        bool operator==(State2 const&) const = default;
+        auto operator<=>(State2 const&) const = default;
+    };
+
+    MultiSet<State> queue;
+    Map<State2, i64> scores;
+
+    auto add = [&](State state) {
+        if (state.row >= lines.size() || state.col >= lines[0].size()) {
+            return;
+        }
+
+        if (lines[state.row][state.col] == '#') {
+            return;
+        }
+
+        auto state2 = State2 { state.row, state.col, state.dr, state.dc };
+        auto& score_map = scores[state2];
+        if (score_map == 0 || state.score <= score_map) {
+            state.tiles.insert({ state.row, state.col });
+            queue.insert(di::move(state));
+            score_map = state.score;
+        }
+    };
+
+    add({ 1, {}, srow, scol, 0, 1 }); // east
+
+    auto fscore = Optional<i64> {};
+    auto fset = Set<Coord> {};
+    while (!queue.empty()) {
+        auto state = di::move(*queue.begin());
+        queue.erase(queue.begin());
+        if (state.row == erow && state.col == ecol) {
+            if (fscore.has_value() && state.score > *fscore) {
+                break;
+            }
+
+            fscore = state.score;
+            for (auto tile : state.tiles) {
+                fset.insert(tile);
+            }
+            continue;
+        }
+        if (fscore.has_value() && state.score >= fscore.value()) {
+            break;
+        }
+
+        // Turns
+        add({ state.score + 1000, clone(state.tiles), state.row, state.col, state.dc, state.dr });
+        add({ state.score + 1000, clone(state.tiles), state.row, state.col, -state.dc, -state.dr });
+
+        add({ state.score + 1, clone(state.tiles), state.row + state.dr, state.col + state.dc, state.dr, state.dc });
+    }
+
+    return fset.size();
+}
